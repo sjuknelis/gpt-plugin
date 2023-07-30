2c9016770e8e4742b1418e7d4278885dcf571b93
Simas2006 <sjuknelis@gmail.com>
2023-07-29 21:55:07 -0400

    add finance functions

--- src/routes/FinancialChatbot.js
+++ src/routes/FinancialChatbot.js
@@ -1,17 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TaskList from "../components/TaskList";
+import ChatbotInterface from "../components/ChatbotInterface";
+import { gptRequest, messageHistory } from "../gpt";
+import useSettings from "../hooks/useSettings";
 
 export default function FinancialChatbot() {
+    const [settings,setSettings] = useSettings();
+
+    useEffect(() => {
+        messageHistory = [
+            {role: "system", content: `You are an assistant helping a freelancer to manage their financial arrangements with a client. Here is the freelancer's resume: ${settings.resume}`},
+            {role: "system", content: `Here is the job description: ${settings.jobDescription}`}
+        ];
+    },[]);
+
     const [tasks,setTasks] = useState({
-        "Rates": false,
-        "Terms": false,
-        "Invoices": false
+        "Negotiating rates": false,
+        "Setting terms of payment": false,
+        "Writing/sending invoices": false
     });
     const [selectedKey,setSelectedKey] = useState(null);
 
     return (
         <>
-            <TaskList tasks={tasks} setTasks={setTasks} />
+            <TaskList tasks={tasks} setTasks={setTasks} selectedKey={selectedKey} setSelectedKey={setSelectedKey} />
+            <ChatbotInterface
+                questionPlaceholder="Finance question from client (leave blank to create prompt)..."
+                choiceFormula={async question => {
+                    return await gptRequest(``);
+                }}
+                answerFormula={async choice => {
+
+                }}
+            />
         </>
     );
 }
